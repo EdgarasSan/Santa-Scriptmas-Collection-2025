7c9c99cffad574c9787400b236b7e44a75609365
Edgar <edgaraskontautas352@gmail.com>
2025-12-16 02:03:03 +0200

    Delete santa-trimmed-number.js

--- santa-trimmed-number.js
+++
@@ -1,8 +0,0 @@
-function trimNumber(num) {
-  const s = num.toString();
-  return Number(s[0] + s[3]);
-}
-
-console.log(trimNumber(1235)); // 15
-console.log(trimNumber(9876)); // 96
-console.log(trimNumber(8765)); // 85
